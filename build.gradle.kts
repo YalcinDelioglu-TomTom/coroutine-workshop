980182190610412df96dbfe60acd1dd01ffca15c
Yalcin Delioglu <yalcin.delioglu@tomtom.com>
2021-04-06 21:29:59 +0200

    Logback is added.

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,10 @@ repositories {
 dependencies {
     val coroutinesVersion = "1.4.2"
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:$coroutinesVersion")
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-slf4j:$coroutinesVersion")
+
+    implementation ("ch.qos.logback:logback-classic:1.0.13")
     testImplementation(kotlin("test-junit"))
 }
 
